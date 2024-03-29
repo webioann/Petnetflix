885aaad0e86914e77cfeebee3f099b3a9101dbfb
Webioann <ioannn7777@gmail.com>
2024-03-29 08:28:37 +0200

    console log removed

--- lib/getTrailerVideoURL.ts
+++ lib/getTrailerVideoURL.ts
@@ -12,10 +12,7 @@ export default async function getTrailerVideoURL(media_type: Media_Type, movie_i
     })
     if(!serverResponse.ok) throw new Error('Failed to fetch data for YouTube trailer URL')
     let response: Promise<IVideoDataResponse> = serverResponse.json()
-
     const results = (await response).results
-    console.log('RESPONSE --->', results)
-
     const index = results.findIndex((element ) => element.type === 'Trailer')
     const key = results[index]?.key
     
