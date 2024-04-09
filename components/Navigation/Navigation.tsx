e1aa9c715a57608ccb77ec6bb26ad0ee1d2caf69
Webioann <ioannn7777@gmail.com>
2024-04-09 11:31:24 +0300

    active class for Navigation links

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useContext } from 'react'
 import { UserContext } from '../../context/UserContext'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import './navigation.scss'
 
@@ -9,15 +10,29 @@ const Navigation = () => {
     const {user} = useContext(UserContext)
     // slice user id and pass as params in MyListPage component (user_id it is document ref in Firebase store DB)
     const shorted_user_id = user?.user_id?.slice(0, 8)
+    const pathname = usePathname()
 
     return (
         <nav className='nav'>
-            <Link className='nav-link home-link' href={'/browse'}>Home</Link>
-            <Link className='nav-link' href={'/tv_shows'}>TV Shows</Link>
-            <Link className='nav-link' href={'/movies'}>Movies</Link>
-            { user && <Link 
-                className='nav-link'
-                href={`/my_list/${shorted_user_id}`}>
+            <Link 
+                className={pathname === '/browse' ? 'active-link home-link' : 'nav-link home-link'}
+                href={'/browse'}>
+                Home
+            </Link>
+            <Link 
+                className={pathname === '/tv_shows' ? 'active-link' : 'nav-link'} 
+                href={'/tv_shows'}>
+                TV Shows
+            </Link>
+            <Link 
+                className={pathname === '/movies' ? 'active-link' : 'nav-link'} 
+                href={'/movies'}>
+                Movies
+            </Link>
+            { user && 
+                <Link 
+                    className='nav-link'
+                    href={`/my_list/${shorted_user_id}`}>
                     My List
                 </Link> 
             }
